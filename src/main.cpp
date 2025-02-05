92081e73c44117d2c5bbafbe220ac4f302c8f160
Luca Olivetti <luca@ventoso.org>
2025-02-05 18:45:30 +0100

    Change settings for the bmp280

--- src/main.cpp
+++ src/main.cpp
@@ -50,10 +50,10 @@ void setup() {
   } else {
     bmpok=true;
 
-    /* Default settings from datasheet. */
-    bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
-                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+    /* Settings for standard resolution from datasheet. */
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                    Adafruit_BMP280::SAMPLING_X1,     /* Temp. oversampling */
+                    Adafruit_BMP280::SAMPLING_X4,     /* Pressure oversampling */
                     Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                     Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
   }
